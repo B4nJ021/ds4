8fbd6f545e3378abed248273680a6a59bc74fec9
B4nJ021 <carloselreal2110@gmail.com>
2025-09-16 20:04:51 -0500

    Ejercicio 3 completado

---
+++ Laboratorio33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PerimetroRectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el largo del rectángulo: ");
+            double largo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce el ancho del rectángulo: ");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = 2 * (largo + ancho);
+
+            Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
+        }
+    }
+}
