be1b55c8cb03eafbf4d8bc7896e530e65b412107
B4nJ021 <carloselreal2110@gmail.com>
2025-10-03 05:23:39 -0500

    Lab 8-6 completado

---
+++ Laboratorio86/Program.cs
@@ -0,0 +1,26 @@
+﻿class ClaseBase
+{
+    public void test()
+    {
+
+    }
+    public virtual void masTests() //agregar virtual
+    {
+
+    }
+}
+
+class ClaseHijo : ClaseBase
+{
+    public override void masTests()
+    {
+
+    }
+}
+internal class Program
+{
+       private static void Main(string[] args)
+    {
+        Console.WriteLine("Corrio la aplicacion");
+    }
+}
