e61aa1c4f69484a428c8845ee14a01219233a516
B4nJ021 <carloselreal2110@gmail.com>
2025-10-03 06:03:55 -0500

    Problemas #4 y #5 completados

---
+++ Laboratorio95/Aleatorio.cs
@@ -0,0 +1,21 @@
+﻿class Aleatorio
+{
+    private Random rand = new Random();
+
+    // Generar número entre dos valores
+    public int GenerarNumero(int min, int max)
+    {
+        return rand.Next(min, max + 1); // incluye max
+    }
+
+    // Generar arreglo de números entre dos valores
+    public int[] GenerarArreglo(int cantidad, int min, int max)
+    {
+        int[] arreglo = new int[cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = rand.Next(min, max + 1);
+        }
+        return arreglo;
+    }
+}
---
+++ Laboratorio95/Program.cs
@@ -0,0 +1,36 @@
+﻿class Programa5
+{
+    public static void Main(string[]args)
+    {
+        Aleatorio ale = new Aleatorio();
+
+        Console.Write("Cantidad de números a generar: ");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        Console.Write("Número mínimo: ");
+        int min = int.Parse(Console.ReadLine());
+
+        Console.Write("Número máximo: ");
+        int max = int.Parse(Console.ReadLine());
+
+        if (cantidad > (max - min + 1))
+        {
+            Console.WriteLine("No se pueden generar números únicos en ese rango.");
+            return;
+        }
+
+        HashSet<int> numeros = new HashSet<int>();
+
+        while (numeros.Count < cantidad)
+        {
+            numeros.Add(ale.GenerarNumero(min, max));
+        }
+
+        Console.WriteLine("Números generados sin repetir:");
+        foreach (int n in numeros)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+    }
+}
