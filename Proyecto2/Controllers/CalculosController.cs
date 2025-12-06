9a772577204cd7f826aa0c56c0dd0e770a7a638a
B4nJ021 <carloselreal2110@gmail.com>
2025-12-05 23:56:17 -0500

    Proyecto 2 completado

---
+++ Proyecto2/Controllers/CalculosController.cs
@@ -0,0 +1,104 @@
+﻿using Proyecto2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Proyecto2.Controllers
+{
+    [RoutePrefix("api/calculos")]
+    public class CalculosController : ApiController
+    {
+        private readonly ICalculoRepository _repository;
+
+        public CalculosController()
+        {
+            
+            string connectionString = @"Server=localhost;Database=CalculadoraDB;Integrated Security=true;";
+            
+
+            _repository = new CalculoRepository(connectionString);
+        }
+
+        // GET: api/calculos
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetTodosLosCalculos()
+        {
+            try
+            {
+                var calculos = _repository.GetTodosLosCalculos();
+                return Ok(calculos);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET: api/calculos/sumas
+        [HttpGet]
+        [Route("sumas")]
+        public IHttpActionResult GetSumas()
+        {
+            try
+            {
+                var sumas = _repository.GetSumas();
+                return Ok(sumas);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET: api/calculos/restas
+        [HttpGet]
+        [Route("restas")]
+        public IHttpActionResult GetRestas()
+        {
+            try
+            {
+                var restas = _repository.GetRestas();
+                return Ok(restas);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET: api/calculos/multiplicaciones
+        [HttpGet]
+        [Route("multiplicaciones")]
+        public IHttpActionResult GetMultiplicaciones()
+        {
+            try
+            {
+                var multiplicaciones = _repository.GetMultiplicaciones();
+                return Ok(multiplicaciones);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET: api/calculos/divisiones
+        [HttpGet]
+        [Route("divisiones")]
+        public IHttpActionResult GetDivisiones()
+        {
+            try
+            {
+                var divisiones = _repository.GetDivisiones();
+                return Ok(divisiones);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
---
+++ Proyecto2/Repositories/CalculoRepository.cs
@@ -0,0 +1,106 @@
+﻿using Proyecto2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto2.Repositories
+{
+    public class CalculoRepository : ICalculoRepository
+    {
+        private readonly string connectionString;
+
+        public CalculoRepository(string connString)
+        {
+            connectionString = connString;
+        }
+
+        public List<CalculoModel> GetTodosLosCalculos()
+        {
+            List<CalculoModel> calculos = new List<CalculoModel>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"SELECT Id, Operacion, Resultado, FechaHora 
+                               FROM HistorialCalculos 
+                               ORDER BY FechaHora DESC";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            calculos.Add(new CalculoModel
+                            {
+                                Id = reader.GetInt32(0),
+                                Operacion = reader.GetString(1),
+                                Resultado = reader.GetDecimal(2),
+                                FechaHora = reader.GetDateTime(3)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return calculos;
+        }
+
+        public List<CalculoModel> GetSumas()
+        {
+            return GetCalculosPorOperador("+");
+        }
+
+        public List<CalculoModel> GetRestas()
+        {
+            return GetCalculosPorOperador("-");
+        }
+
+        public List<CalculoModel> GetMultiplicaciones()
+        {
+            return GetCalculosPorOperador("*");
+        }
+
+        public List<CalculoModel> GetDivisiones()
+        {
+            return GetCalculosPorOperador("/");
+        }
+
+        private List<CalculoModel> GetCalculosPorOperador(string operador)
+        {
+            List<CalculoModel> calculos = new List<CalculoModel>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"SELECT Id, Operacion, Resultado, FechaHora 
+                               FROM HistorialCalculos 
+                               WHERE Operacion LIKE '%' + @Operador + '%'
+                               ORDER BY FechaHora DESC";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Operador", operador);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            calculos.Add(new CalculoModel
+                            {
+                                Id = reader.GetInt32(0),
+                                Operacion = reader.GetString(1),
+                                Resultado = reader.GetDecimal(2),
+                                FechaHora = reader.GetDateTime(3)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return calculos;
+        }
+    }
+}
